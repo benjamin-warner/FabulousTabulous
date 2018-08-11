be24bd76b30bfb8d0a3cf47de919e51286b7d53c
Ben Warner <benjaminnwarner@gmail.com>
2018-08-11 10:57:31 -0400

    Added gulp gh pages stuff

--- src/store/modules/tab.js
+++ src/store/modules/tab.js
@@ -24,7 +24,7 @@ const state = {
     '0':{
       id: '0',
       parentId: '0',
-      beats: ['0', '1']
+      beats: ['0', '1', '2', '3']
     }
   },
   beats: {
@@ -38,21 +38,45 @@ const state = {
       parentId: '0',
       notes: ['6','7','8','9','10','11']
     },
+    '2':{
+      id:'2',
+      parentId:'0',
+      notes: ['12','13','14','15','16','17']
+    },
+    '3':{
+      id:'3',
+      parentId: '0',
+      notes: ['18','19','20','21','22','23']
+    }
   },
   notes: {
-    '0': { parentId: '0', id: '0', note: '0'},
-    '1': { id: '1', parentId: '0', note: '1'},
-    '2': { id: '2', parentId: '0', note: '2'},
-    '3': { id: '3', parentId: '0', note: '3'},
-    '4': { id: '4', parentId: '0', note: '4'},
-    '5': { id: '5', parentId: '0', note: '5'},
+    '0': { parentId: '0', id: '0', note: ''},
+    '1': { id: '1', parentId: '0', note: ''},
+    '2': { id: '2', parentId: '0', note: ''},
+    '3': { id: '3', parentId: '0', note: ''},
+    '4': { id: '4', parentId: '0', note: ''},
+    '5': { id: '5', parentId: '0', note: ''},
 
-    '6': { id: '6', parentId: '1', note: '6'},
-    '7': { id: '7', parentId: '1', note: '7'},
-    '8': { id: '8', parentId: '1', note: '8'},
-    '9': { id: '9', parentId: '1', note: '9'},
-    '10': { id: '10', parentId: '1', note: '10'},
-    '11': { id: '11', parentId: '1', note: '11'}
+    '6': { id: '6', parentId: '1', note: ''},
+    '7': { id: '7', parentId: '1', note: ''},
+    '8': { id: '8', parentId: '1', note: ''},
+    '9': { id: '9', parentId: '1', note: ''},
+    '10': { id: '10', parentId: '1', note: ''},
+    '11': { id: '11', parentId: '1', note: ''},
+
+    '12': { id: '12', parentId: '2', note: ''},
+    '13': { id: '13', parentId: '2', note: ''},
+    '14': { id: '14', parentId: '2', note: ''},
+    '15': { id: '15', parentId: '2', note: ''},
+    '16': { id: '16', parentId: '2', note: ''},
+    '17': { id: '17', parentId: '2', note: ''},
+
+    '18': { id: '18', parentId: '3', note: ''},
+    '19': { id: '19', parentId: '3', note: ''},
+    '20': { id: '20', parentId: '3', note: ''},
+    '21': { id: '21', parentId: '3', note: ''},
+    '22': { id: '22', parentId: '3', note: ''},
+    '23': { id: '23', parentId: '3', note: ''}
   }
 }
 
